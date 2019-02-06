a8a0c7fe547b3b7549fcc4c0f1249b59fb7e5667
Agnelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-02-06 05:59:25 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,8 +21,7 @@ def bonus
   }
 
   #code your solution here:
-  epic_tragedy[:montague][:patriarch][:matriarch][:hero]["status"].change_to
-  [:name][:age][:status] = "dead"
+
   
     #epic_tragedy[:capulet][:patriarch][:matriarch][:heroine][:status] = "dead"  #["name"]["age"]["status"] = "dead"
   
